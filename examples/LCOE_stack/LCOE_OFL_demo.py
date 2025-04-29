3e82f3a4ea0490097891ef6e0f5ce2b00989e09c
Cory Frontin <cory.frontin@nrel.gov>
2025-04-29 10:07:19 -0600

    capitalize ORBIT acronym everywhere

--- examples/LCOE_stack/LCOE_OFL_demo.py
+++ examples/LCOE_stack/LCOE_OFL_demo.py
@@ -48,8 +48,8 @@ prob = glue.create_setup_OM_problem(
 
 if False:  # set true to run one-shot analysis
 
-    # setup the latent variables for Orbit and FinanceSE
-    cost_wisdem.Orbit_setup_latents(prob, modeling_options)
+    # setup the latent variables for ORBIT and FinanceSE
+    cost_wisdem.ORBIT_setup_latents(prob, modeling_options)
     cost_wisdem.FinanceSE_setup_latents(prob, modeling_options)
 
     # set up the working/design variables
@@ -107,8 +107,8 @@ else:
     # setup the problem
     prob.setup()
 
-    # setup the latent variables for Orbit and FinanceSE
-    cost_wisdem.Orbit_setup_latents(prob, modeling_options)
+    # setup the latent variables for ORBIT and FinanceSE
+    cost_wisdem.ORBIT_setup_latents(prob, modeling_options)
     cost_wisdem.FinanceSE_setup_latents(prob, modeling_options)
 
     # set up the working/design variables
