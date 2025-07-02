b0bb9c94645884b2ecfc61ea04a3c453e6740d8d
Cory Frontin <cory.frontin@nrel.gov>
2025-07-02 10:50:47 -0600

    a little more improvement to the test utils

--- ard/utils/test_utils.py
+++ ard/utils/test_utils.py
@@ -7,7 +7,7 @@ import traceback
 
 
 def pyrite_validator(
-    validation_data: dict,
+    data_for_validation: dict,
     filename_pyrite: PathLike,
     rewrite: bool = False,
     rtol_val: float = 1e-6,
@@ -23,7 +23,7 @@ def pyrite_validator(
         # this helper function can write a file to hold pyrite-standard data
 
         # write out a npz file that holds the variables we want to be able to check
-        np.savez(filename_pyrite, **validation_data)
+        np.savez(filename_pyrite, **data_for_validation)
         assert False
     else:
         # ... or it can check to make sure that an existing pyrite file matches the current data
@@ -36,9 +36,9 @@ def pyrite_validator(
         for k, v in pyrite_data.items():
             # count how many of the values in the data match the equivalent validation data
             sum_isclose = np.sum(
-                np.isclose(np.array(v), validation_data[k], rtol=rtol_val)
+                np.isclose(np.array(v), data_for_validation[k], rtol=rtol_val)
             )
-            vd_size = np.array(validation_data[k], dtype=np.float64).size
+            vd_size = np.array(data_for_validation[k], dtype=np.float64).size
             # assert all of the values match
             validation_matches = sum_isclose == vd_size
 
@@ -49,8 +49,8 @@ def pyrite_validator(
                     file=sys.stderr,
                 )
                 print(f"\tto a tolerance of {rtol_val:e}", file=sys.stderr)
-                print(f"saved data for {k}: {v}", file=sys.stderr)
-                print(f"computed data for {k}: {validation_data[k]}", file=sys.stderr)
+                print(f"pyrite data for {k}: {v}", file=sys.stderr)
+                print(f"computed data for {k}: {data_for_validation[k]}", file=sys.stderr)
                 stack = traceback.format_stack(limit=3)
                 print("".join(stack[-2]).replace("  File", "test:"), file=sys.stderr)
                 print(file=sys.stderr)
