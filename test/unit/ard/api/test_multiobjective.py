ab6414fa00508385d883b53c9fd55bdabd4f72a8
Cory Frontin <cory.frontin@nrel.gov>
2025-10-22 13:28:37 -0600

    improved unit testing

--- test/unit/ard/api/test_multiobjective.py
+++ test/unit/ard/api/test_multiobjective.py
@@ -12,55 +12,69 @@ class TestMultiobjectiveSetUp:
     def setup_method(self):
 
         # create the simplest system that will compile
-        input_dict = load_yaml(
+        input_dict = self.input_dict = load_yaml(
             Path(__file__).parent / "inputs_onshore" / "ard_system_moo.yaml"
         )
-        input_dict["system"] = {
-            "type": "group",
-            "systems": {
-                "layout": {
-                    "type": "component",
-                    "module": "ard.layout.gridfarm",
-                    "object": "GridFarmLayout",
-                    "promotes": ["*"],
-                    "kwargs": {
-                        "modeling_options": input_dict["modeling_options"],
-                    },
-                },
-                "boundary": {
-                    "type": "component",
-                    "module": "ard.layout.boundary",
-                    "object": "FarmBoundaryDistancePolygon",
-                    "promotes": ["*"],
-                    "kwargs": {
-                        "modeling_options": input_dict["modeling_options"],
-                    },
-                },
-                "aepFLORIS": {
-                    "type": "component",
-                    "module": "ard.farm_aero.floris",
-                    "object": "FLORISAEP",
-                    "promotes": ["x_turbines", "y_turbines", "AEP_farm"],
-                    "kwargs": {
-                        "modeling_options": input_dict["modeling_options"],
-                        # "data_path":
-                        "case_title": "default",
-                    },
-                },
-            },
-        }
 
         # create an ard model
         self.da_plough = set_up_ard_model(
             input_dict=input_dict,
         )
 
-    def test_raise_multiobjective(self):
+    def test_response_variables(self, subtests):
+
+        # preemptively run the final setup
+        self.da_plough.final_setup()
+
+        # extract the response vars
+        response_vars = self.da_plough.list_driver_vars(out_stream=None)
+
+        # the expected responses
+        DVs_expected = {
+            "spacing_primary",
+            "spacing_secondary",
+            "angle_orientation",
+            "angle_skew",
+        }
+        constrs_expected = set()
+        objs_expected = {
+            "AEP_farm",
+            "landuse.area_tight",
+        }
+
+        # use set equivalence to make sure the OM problem matches expectations
+        with subtests.test("design vars"):
+            assert (
+                set([v[0] for v in response_vars["design_vars"]]) == DVs_expected
+            ), "design vars must match"
+        with subtests.test("constraints"):
+            assert (
+                set([v[0] for v in response_vars["constraints"]]) == constrs_expected
+            ), "constraints must match"
+        with subtests.test("objectives"):
+            assert (
+                set([v[0] for v in response_vars["objectives"]]) == objs_expected
+            ), "objectives must match"
+
+    def test_raise_scipy_MOO_error(self):
+
+        self.input_dict["analysis_options"]["driver"]["name"] = "ScipyOptimizeDriver"
+        self.input_dict["analysis_options"]["driver"]["options"]["optimizer"] = "COBYLA"
+        self.input_dict["analysis_options"]["driver"]["options"]["opt_settings"] = {
+            "rhobeg": 2.0,
+            "maxiter": 50,
+        }
+
+        # re-create an ard model
+        self.da_plough = set_up_ard_model(
+            input_dict=self.input_dict,
+        )
 
         # make sure the driver runs and gets the scipy error
         with pytest.raises(
             RuntimeError,
             match="ScipyOptimizeDriver currently does not support multiple objectives.",
         ):
+
             # attempt to run the driver
             self.da_plough.run_driver()
